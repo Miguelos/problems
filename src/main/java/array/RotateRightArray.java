d9e9040ab17d591c42bc29516183380517915ef5
Miguel Gonzalez Perez <me@miguelos.me>
2019-07-16 12:32:37 +0200

    Array rotate right problem

---
+++ src/main/java/array/RotateRightArray.java
@@ -0,0 +1,33 @@
+package array;
+
+public class RotateRightArray {
+
+  public static void main(String[] args) {
+    rotateArray(new int[]{1, 2, 3, 4, 5, 6}, 4);
+  }
+
+  /**
+   * Recursive solution.
+   * <p>
+   * Time complexity: O(N) Space complexity: O(1)
+   */
+  public static void rotateArray(int[] nums, int k) {
+    if (nums.length <= 1) {
+      return;
+    }
+
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k % nums.length - 1);
+    reverse(nums, k % nums.length, nums.length - 1);
+  }
+
+  private static void reverse(int[] nums, int i, int j) {
+    while (i < j) {
+      int aux = nums[j];
+      nums[j] = nums[i];
+      nums[i] = aux;
+      i++;
+      j--;
+    }
+  }
+}
