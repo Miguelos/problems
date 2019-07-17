f821934d1deed7316df252a240d9cb6aa6fca0ef
Miguel Gonzalez Perez <me@miguelos.me>
2019-07-17 10:23:35 +0200

    Buy sell stock n-days & gitignore update

--- src/main/java/dinamicProgramming/MaximizeProfit.java
+++ src/main/java/dinamicProgramming/MaximizeProfit.java
@@ -1,23 +1,26 @@
 package dinamicProgramming;
 
-/**
- * Say you have an array for which the ith element is the price of a given stock on day i. If you
- * were only permitted to complete at most one transaction (i.e., buy one and sell one share of the
- * stock), design an algorithm to find the maximum profit. Note that you cannot sell a stock before
- * you buy one.
- */
 public class MaximizeProfit {
 
   public static void main(String[] args) {
-    maxProfit(new int[]{7, 1, 5, 3, 6, 4});
+    maxProfit1(new int[]{7, 1, 5, 3, 6, 4});
+    maxProfitN(new int[]{7, 1, 5, 3, 6, 4});
   }
 
   /**
-   * <p>
+   * Best Time to Buy and Sell Stock. k = 1 (buy and sell at most once).
+   *
+   * Say you have an array for which the ith element is the price of a given stock on day i. If you
+   * were only permitted to complete at most one transaction (i.e., buy one and sell one share of
+   * the stock), design an algorithm to find the maximum profit. Note that you cannot sell a stock
+   * before you buy one.
+   *
    * Time complexity: O(N). Space complexity: O(1).
-   * </p>
+   *
+   * @param prices array with p
+   * @return maximum profit
    */
-  public static int maxProfit(int[] prices) {
+  public static int maxProfit1(int[] prices) {
     if (prices == null || prices.length <= 1) {
       return 0;
     }
@@ -29,5 +32,54 @@ public class MaximizeProfit {
     }
 
     return maxProfit;
+  }
+
+  /**
+   * Best Time to Buy and Sell Stock II. k = 0..n (buy and sell multiple times).
+   *
+   * Say you have an array for which the ith element is the price of a given stock on day i.
+   *
+   * Design an algorithm to find the maximum profit. You may complete as many transactions as you
+   * like (i.e., buy one and sell one share of the stock multiple times).
+   *
+   * Note: You may not engage in multiple transactions at the same time (i.e., you must sell the
+   * stock before you buy again).
+   *
+   * Time complexity: O(N). Space complexity: O(1).
+   *
+   * @param prices array with p
+   * @return maximum profit
+   */
+  public static int maxProfitN(int[] prices) {
+    if (prices == null || prices.length <= 1) {
+      return 0;
+    }
+
+    int profit = 0;
+    int buyPrice = -1;
+
+    if (prices[0] < prices[1]) {
+      buyPrice = prices[0];
+      profit -= prices[0];
+    }
+
+    for (int i = 1; i < prices.length - 1; i++) {
+      int comparison = prices[i] - prices[i + 1];
+      if (comparison > 0) {
+        if (buyPrice != -1) {
+          profit += prices[i];
+          buyPrice = -1;
+        }
+      } else if (comparison < 0 && buyPrice == -1) {
+        profit -= prices[i];
+        buyPrice = prices[i];
+      }
+    }
+
+    if (buyPrice != -1 && prices[prices.length - 1] > buyPrice) {
+      profit += prices[prices.length - 1];
+    }
+
+    return profit;
   }
 }
--- src/test/groovy/dinamicProgramming/MaximizeProfitTest.groovy
+++ src/test/groovy/dinamicProgramming/MaximizeProfitTest.groovy
@@ -2,11 +2,22 @@ package dinamicProgramming
 
 class MaximizeProfitTest extends GroovyTestCase {
 
-  void testMaxProfit() {
+  void testMaxProfit1() {
     def testArray = [7, 1, 5, 3, 6, 4] as int[]
-    assert MaximizeProfit.maxProfit(testArray) == 5
+    assert MaximizeProfit.maxProfit1(testArray) == 5
 
     testArray = [7, 6, 4, 3, 1] as int[]
-    assert MaximizeProfit.maxProfit(testArray) == 0
+    assert MaximizeProfit.maxProfit1(testArray) == 0
+  }
+
+  void testMaxProfitN() {
+    def testArray = [7, 1, 5, 3, 6, 4] as int[]
+    assert MaximizeProfit.maxProfitN(testArray) == 7
+
+    testArray = [1, 2, 3, 4, 5] as int[]
+    assert MaximizeProfit.maxProfitN(testArray) == 4
+
+    testArray = [7, 6, 4, 3, 1] as int[]
+    assert MaximizeProfit.maxProfitN(testArray) == 0
   }
 }
